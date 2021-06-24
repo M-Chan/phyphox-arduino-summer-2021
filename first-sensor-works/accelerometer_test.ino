d396907ffd18f572de39d531b96be5ba10d2d940
Man Ting Chan <81781462+M-Chan@users.noreply.github.com>
2021-06-24 10:27:12 +0100

    Update accelerometer_test.ino
    fixed problems uploading sketches and removed useless pieces of code

--- first-sensor-works/accelerometer_test.ino
+++ first-sensor-works/accelerometer_test.ino
@@ -1,28 +1,14 @@
 /*
 Test to access the Arduino's accelerometer
 */
+
 #include <phyphoxBle.h>
 #include <Arduino_LSM9DS1.h>
 
-//float x, y, z;
 
 void setup() {
   // put your setup code here, to run once:
-
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Started");
-
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
-
-  Serial.print("Accelerometer sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println(" Hz");
  
-  
   PhyphoxBLE::start();
 
   PhyphoxBleExperiment accelerometerExp; //This is to generate my experiment
@@ -44,6 +30,10 @@ void setup() {
 
   accelerometerExp.addView(firstView); //Adds the view to the experiment
   PhyphoxBLE::addExperiment(accelerometerExp); //Attaches this experiment to the server for writing the data to the Arduino
+
+  if (!IMU.begin()) {
+    while (1);
+  }
 }
 
 void loop() {
@@ -54,9 +44,9 @@ void loop() {
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
   }
+  
   x = 100*x;
-  float degreesX = map(x, 0, -100, 0, 90);
-  Serial.print(degreesX);
+
   PhyphoxBLE::write(x,y,z);
   delay(1000);
 }
